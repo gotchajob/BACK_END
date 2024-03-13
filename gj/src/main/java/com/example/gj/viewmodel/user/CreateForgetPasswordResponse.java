37b6547895d801ad8eaf2c49b5104390299a839d
Chivvse151512 <97072169+Chivvse151512@users.noreply.github.com>
2024-03-13 11:02:24 +0700

    add response at /create-forget-password

---
+++ gj/src/main/java/com/example/gj/viewmodel/user/CreateForgetPasswordResponse.java
@@ -0,0 +1,10 @@
+package com.example.gj.viewmodel.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class CreateForgetPasswordResponse {
+    private String code;
+}
